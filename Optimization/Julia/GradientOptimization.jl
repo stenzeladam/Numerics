2d6f596cad2b2b4d38fa24a0ab4883f76bb039f9
Adam Stenzel <stenzeladam@gmail.com>
2025-01-18 22:23:20 -0800

    Added to GradientOptimization.jl

--- Optimization/Julia/GradientOptimization.jl
+++ Optimization/Julia/GradientOptimization.jl
@@ -15,8 +15,8 @@ function fplot(f)
     return
 end
 
-fplot(f)
-show() 
+# fplot(f)
+# show()
 
 function df(x)
     a1 = x[1]^2/2 - x[2]^2/4 + 3
@@ -37,7 +37,6 @@ function gradient_descent(f, df, x0, α=0.1; tol=1e-4, maxiter=500)
         x -= α*gradient
         push!(xs, x)
     end
-    println("\nxs: ", xs)
     return xs
 end
 
@@ -48,11 +47,132 @@ end
 
 x0s = [[-2,.5], [0,.5], [2.2,-0.5]];
 
-fplot(f)
-title("Gradient descent, fixed \$\\alpha\$")
-for x0 in x0s
-    xs = gradient_descent(f, df, x0, 0.3)
-    plot_path(xs)
-    #println("Path length = $(length(xs)), ||gradient|| = $(sqrt(sum(df(xs[end]).^2)))")
+# Plotting gradient descent
+
+#fplot(f)
+# title("Gradient descent, fixed \$\\alpha\$")
+# println("Gradient descent, fixed α")
+# for x0 in x0s
+#     xs = gradient_descent(f, df, x0, 0.3)
+#     plot_path(xs)
+#     println("Path length = $(length(xs)), ||gradient|| = $(sqrt(sum(df(xs[end]).^2)))")
+# end
+# print("\n")
+#show()
+
+function line_search(f, direction, x, αmin=1/2^20, αmax=2^20)
+    α = αmin
+    fold = f(x)
+    while true
+        if α ≥ αmax
+            return α
+        end
+        fnew = f(x - α*direction)
+        if fnew ≥ fold
+            return α/2
+        else
+            fold = fnew
+        end
+        α *= 2
+    end
 end
+
+function gradient_descent_line_search(f, df, x0; tol=1e-4, maxiter=500)
+    x = x0
+    xs = [x0]
+    for i = 1:maxiter
+        gradient = df(x)
+        if sqrt(sum(gradient.^2)) < tol
+            break
+        end
+        α = line_search(f, gradient, x)
+        x -= α*gradient
+        push!(xs, x)
+    end
+    return xs
+end
+
+# Plotting line search, to improve gradient descent
+
+# fplot(f)
+# title("Gradient descent, line searches for \$\\alpha\$")
+# println("Gradient descent, line searches for α")
+# for x0 in x0s
+#     xs = gradient_descent_line_search(f, df, x0)
+#     plot_path(xs)
+#     println("Path length = $(length(xs)), ||gradient|| = $(sqrt(sum(df(xs[end]).^2)))")
+# end
+# print("\n")
+# show()
+
+function finite_difference_gradient(f, x, ϵ=1e-5)
+    n = length(x)
+    df = zeros(n)
+    for k = 1:n
+        x1 = copy(x)
+        x1[k] += ϵ
+        fP = f(x1)
+        x1[k] -= 2ϵ
+        fM = f(x1)
+        df[k] = (fP - fM) / 2ϵ
+    end
+    return df
+end
+
+# An alternate to implementing the gradient function df using finite differences
+
+# fplot(f)
+# title("Gradient descent, line searches for \$\\alpha\$, numerical gradients")
+# println("Gradient descent, line searches for α, numerical gradients")
+# for x0 in x0s
+#     num_df(x) = finite_difference_gradient(f, x)
+#     xs = gradient_descent_line_search(f, num_df, x0)
+#     plot_path(xs)
+#     println("Path length = $(length(xs)), ||gradient|| = $(sqrt(sum(df(xs[end]).^2)))")
+# end
+# print("\n")
+# show()
+
+# Newton's method
+
+function finite_difference_hessian(f, x, ϵ=1e-5)
+    n = length(x)
+    ddf = zeros(n,n)
+    f0 = f(x)
+    for i = 1:n
+        x1 = copy(x)
+        x1[i] += ϵ
+        fP = f(x1)
+        x1[i] -= 2ϵ
+        fM = f(x1)
+        ddf[i,i] = (fP - 2*f0 + fM) / ϵ^2
+    end
+    for i = 1:n, j = i+1:n
+        x1 = copy(x)
+        x1[i] += ϵ
+        x1[j] += ϵ
+        fPP = f(x1)
+        x1[i] -= 2ϵ
+        fMP = f(x1)
+        x1[j] -= 2ϵ
+        fMM = f(x1)
+        x1[i] += 2ϵ
+        fPM = f(x1)
+        ddf[i,j] = (fPP - fMP - fPM + fMM) / 4ϵ^2
+        ddf[j,i] = ddf[i,j]
+    end
+    return ddf
+end
+
+fplot(f)
+title("Newton's method, line searches for \$\\alpha\$, numerical gradients and Hessians")
+println("Newton's method, line searches for α, numerical gradients and Hessians")
+x0s = [[-2,0.0], [0,.5], [2.,0.]];
+for x0 in x0s
+    search_dir(x) = finite_difference_hessian(f,x) \ finite_difference_gradient(f, x)
+    xs = gradient_descent_line_search(f, search_dir, x0)
+    plot_path(xs)
+    println("Path length = $(length(xs)), ||gradient|| = $(sqrt(sum(df(xs[end]).^2)))")
+end
+print("\n")
 show()
